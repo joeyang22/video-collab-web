ea46ebf49f87e7578845c789b6fc7eb12a86b75b
Joe Yang <jy22yang@uwaterloo.ca>
2015-12-12 20:19:29 -0500

    added a route

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.json({'asdf':'asdf'});
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening');
+});
