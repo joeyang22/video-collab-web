21208a2ad15d391c7a5fc93e3dbc2941c7f57a45
joeyang22 <jy22yang@uwaterloo.ca>
2016-01-01 14:45:25 -0500

    changed a socket behaviour

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 var playlist = require('./controllers/shareplaylist.js');
-var Room = require('./models/room.js').Room;
+var Room = require('./models/room.js').Room
 
 app.set('port', (process.env.PORT || 5000));
 server.listen(app.get('port'));
@@ -13,7 +13,7 @@ var room = new Room("asdf");
 io.on('connection', function(socket){
   console.log('a user connected');
   playlist.initializeApp(io, socket);
-  socket.on('createNewPlaylist', function(){
+  socket.on('disconnected', function(){
     console.log('user disconnected');
   });
 });
