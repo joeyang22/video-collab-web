1332137a4cca19843200996e02868bfcee1ce33a
Joe Yang <jy22yang@uwaterloo.ca>
2016-02-02 15:47:53 -0500

    pushing to github

--- app.js
+++ app.js
@@ -8,30 +8,6 @@ var Room = require('./models/room.js').Room;
 app.set('port', (process.env.PORT || 5000));
 server.listen(app.get('port'));
 
-//Tests
-// var rm = new Room("asdsa");
-//
-// rm.videos.add(new Video("one"));
-// rm.videos.add(new Video("two"));
-// rm.videos.add(new Video("three"));
-//
-// console.log(rm.videos.peek());
-//
-// rm.videos.forEach(function(video){
-//   if (video.video_id == "one"){
-//     rm.videos.add(new Video("one", video.votes+1));
-//     rm.videos.remove(video);
-//   }else if (video.video_id =="two"){
-//     video.votes+=1;
-//     console.log("wqewq");
-//   }
-// });
-//
-// rm.videos.add(new Video("tester"));
-//
-//
-// console.log(rm.videos.peek());
-
 io.on('connection', function(socket){
   console.log('a user connected');
   playlist.initializeApp(io, socket);
