d4c6fa48f765c7319237cad6dba2f3728568dd82
Joe Yang <jy22yang@uwaterloo.ca>
2016-02-03 22:47:48 -0500

    Logging statements for add/vote video

--- models/video.js
+++ models/video.js
@@ -1,4 +1,5 @@
-exports.Video = function(videoId, votes){
+exports.Video = function(videoId, votes, thumbnailUrl){
   this.video_id= videoId;
   this.votes= votes || 1;
+  this.thumbnailUrl = thumbnailUrl;
 }
